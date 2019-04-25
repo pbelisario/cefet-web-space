a7e3b9484927a74306a7c17884cefe6cc2a7c467
cefet <cefet@cefet.br>
2019-04-25 12:07:49 -0300

    DONE

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,23 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+
+
+    let buttons = document.querySelectorAll(".botao-expandir-retrair");
+    for (let button of buttons)
+    {
+        button.addEventListener("click", (e) => 
+        {
+            if (e.target.innerHTML == '+')
+            {
+                e.target.innerHTML = '-';
+                e.target.closest("p").classList.add("expandido");
+            }
+            else
+            {
+                e.target.innerHTML = '+';
+                e.target.closest("p").classList.remove("expandido");
+            }
+           
+        });
+    }
--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,16 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+  let index = 0;
+  let buttonsControl = document.querySelectorAll(".controle");
+  console.log(buttonsControl);
+    
+  for (let button of buttonsControl)
+  {
+      button.addEventListener("click", (e) => 
+      {
+        const direcao = e.target.id == "anterior" ? +1 : -1; 
+        index = (index + direcao + todasAsImagens.length) % todasAsImagens.length;    
+        e.target.closest("#galeria").querySelector("img").src = servidorDasImagens+todasAsImagens[index];
+      });
+  }
